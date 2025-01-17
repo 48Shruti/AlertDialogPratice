c803e28806b0c5d40b6777814b5070e0130981c2
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2025-01-17 18:12:59 +0300

    Toast changes

--- app/src/main/java/com/shruti/alertdialogpratice/AlertDialog1.kt
+++ app/src/main/java/com/shruti/alertdialogpratice/AlertDialog1.kt
@@ -14,31 +14,36 @@ class AlertDialog1 : AppCompatActivity() {
         binding = ActivityAlertDialog1Binding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnclick.setOnClickListener {
-            val inputText = binding.etnumber.text.toString()
-            count = if(inputText.isNotEmpty()){
-                 inputText.toInt()
-            }else{
-                0
+            if (binding.etnumber.text.isNullOrEmpty()) {
+                binding.etnumber.error = "Enter number"
+                Toast.makeText(this, "Enter number", Toast.LENGTH_SHORT).show()
+            } else {
+                val inputText = binding.etnumber.text.toString()
+                count = if (inputText.isNotEmpty()) {
+                    inputText.toInt()
+                } else {
+                    0
+                }
+                AlertDialog.Builder(this).apply {
+                    setTitle("Add or subtract")
+                    setMessage("If you want to add number then click add or you want to substract then click sub")
+                    setPositiveButton("added") { _, _ ->
+                        count += count
+                        binding.etnumber.setText(count.toString())
+                        Toast.makeText(this@AlertDialog1, "add ", Toast.LENGTH_SHORT).show()
+                    }
+                    setNegativeButton("subtract") { _, _ ->
+                        count -= count
+                        binding.etnumber.setText(count.toString())
+                        Toast.makeText(this@AlertDialog1, "sub ", Toast.LENGTH_SHORT).show()
+                    }
+                    setNeutralButton("reset") { _, _, ->
+                        count = 0
+                        binding.etnumber.setText(count.toString())
+                        Toast.makeText(this@AlertDialog1, "reset", Toast.LENGTH_SHORT).show()
+                    }
+                }.show()
             }
-            AlertDialog.Builder(this).apply {
-                setTitle("Add or subtract")
-                setMessage("If you want to add number then click add or you want to substract then click sub")
-                setPositiveButton("added"){_,_->
-                    count += count
-                    binding.etnumber.setText(count.toString())
-                    Toast.makeText(this@AlertDialog1,"add by 1", Toast.LENGTH_SHORT).show()
-                }
-                setNegativeButton("subtract"){_,_->
-                    count -= count
-                    binding.etnumber.setText(count.toString())
-                    Toast.makeText(this@AlertDialog1,"sub  by 1", Toast.LENGTH_SHORT).show()
-                }
-                setNeutralButton("reset"){_,_,->
-                    count = 0
-                    binding.etnumber.setText(count.toString())
-                    Toast.makeText(this@AlertDialog1,"reset" , Toast.LENGTH_SHORT).show()
-                }
-            }.show()
         }
     }
 }
